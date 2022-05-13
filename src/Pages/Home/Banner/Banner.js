bb4459ae239d1bbbbb44cbf83bfd415944d4ff9f
Muhammad764 <farhadislam745@gmail.com>
2022-05-13 15:10:21 +0600

    banner update

--- src/Pages/Home/Banner/Banner.js
+++ src/Pages/Home/Banner/Banner.js
@@ -1,11 +1,55 @@
-import React from 'react';
-import image from './banner.jpg'
+import React, { useState } from 'react';
+import { Carousel } from 'react-bootstrap';
+import banner from './banner.jpg'
+import banner2 from './banner2.jpg'
+import banner3 from './banner3.jpg'
+
 
 const Banner = () => {
+   
+  const [index, setIndex] = useState(0);
+
+  const handleSelect = (selectedIndex, e) => {
+    setIndex(selectedIndex);
+  };
     return (
-        <div>
-            <img className='w-100 mb-5 mt-5' src={image} alt="" />
-        </div>
+        <div className='mt-5'>
+                  <Carousel activeIndex={index} onSelect={handleSelect}>
+      <Carousel.Item>
+        <img
+          className="d-block w-100 h-50"
+          src={banner}
+          alt="First slide"
+        />
+        <Carousel.Caption>
+          
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="d-block w-100 h-50"
+          src={banner2}
+          alt="Second slide"
+        />
+
+        <Carousel.Caption>
+         
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="d-block w-100 h-50"
+          src={banner3}
+          alt="Third slide"
+        />
+
+        <Carousel.Caption>
+         
+        </Carousel.Caption>
+      </Carousel.Item>
+    </Carousel>
+   </div>
+ 
     );
 };
 
